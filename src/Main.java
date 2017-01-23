8b38324bbd413afc90cefc0b3e1853cce36bced0
Viktor Glazkov <viktorg.dev@gmail.com>
2017-01-23 04:53:57 +0200

    Add vampir numbers printer

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,47 @@
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
+    private static final int ZERO = 0;
+    private static final int ONE = 1;
+    private static final int MIN = 11;
+    private static final int MAX = 100;
+
     public static void main(String[] args) {
-        fibonacci(Integer.valueOf(args[0]));
+        printFibonacciNumbers(7);
+        printVampirNumbers();
     }
 
-    private static void fibonacci(int n) {
-        int beforePrev = 0;
-        int prev = 1;
+    private static void printFibonacciNumbers(int n) {
+        int beforePrev = ZERO;
+        int prev = ONE;
         int current;
 
-        for (int i = 0; i < n; i++) {
+        for (int i = ZERO; i < n; i++) {
             current = beforePrev + prev;
             System.out.print(current + " ");
 
-            if (i != 0) {
+            if (i != ZERO) {
                 beforePrev = prev;
                 prev = current;
+            }
+        }
+
+        System.out.println();
+    }
+
+    private static void printVampirNumbers() {
+        int counter = ONE;
+        for (int i = MIN; i < MAX; i++) {
+            for (int j = i; j < MAX; j++) {
+                int result = i * j;
+
+                List<Integer> fangs = (i + "" + j).chars().mapToObj(c -> c).sorted().collect(Collectors.toList());
+                List<Integer> number = (result + "").chars().mapToObj(c -> c).sorted().collect(Collectors.toList());
+
+                if (number.equals(fangs)) {
+                    System.out.println(counter++ + ") " + result + " = " + i + " * " + j);
+                }
             }
         }
     }
