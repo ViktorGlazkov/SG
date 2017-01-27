09aa69e49776bd285e995d9f19fd928eca21a3ee
Viktor Glazkov <viktorg.dev@gmail.com>
2017-01-27 16:54:58 +0200

    Add trash

--- src/Main.java
+++ src/Main.java
@@ -1,96 +1,66 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
-    private static final int ZERO = 0;
-    private static final int ONE = 1;
-    private static final int MIN = 11;
-    private static final int MAX = 100;
+    private static final int N = 7;
 
     public static void main(String[] args) {
-        printFibonacciNumbers(7);
-        printVampireNumbers();
-        printParserAnalize();
+        List<List<Integer>> lists = new ArrayList<>();
+        Set<Integer> values = new HashSet<>();
+
+        int central = N % 2 == 0 ? N / 2 : (N / 2) + 1;
+
+        Random r = new Random();
+
+        for (int i = 0; i < N * N; i++) {
+            while (values.size() != i + 1) {
+                values.add(r.nextInt(800) + 100);
+            }
+        }
+
+        List<Integer> vals = new ArrayList<>();
+        for (int value: values) {
+            vals.add(value);
+        }
+        for (int i = 0; i < N; i++) {
+            lists.add(vals.subList(i * N, i * N + N));
+        }
+
+        printLists(lists);
+
+        Point point = findMax(lists);
+        int cent = lists.get(central).get(central);
+        lists.get(central).get(central) = lists.get(point.getI()).get(point.getJ());
+        lists.get(point.getI()).get(point.getJ()) = cent;
+
+        printLists(lists);
     }
 
-    private static void printParserAnalize() {
-        long startTime;
-
-        System.out.println("parseInt\tvalueOf");
-        List<Integer> parseInt = new ArrayList<>();
-        List<Integer> valueOf = new ArrayList<>();
-
-        for (int i = ZERO; i < MAX; i++) {
-            startTime = System.nanoTime();
-            Integer.parseInt("12345611", 8);
-            printTime(startTime, parseInt);
-
-            startTime = System.nanoTime();
-            Integer.valueOf("12345611");
-            printTime(startTime, valueOf);
+    private static void printLists(List<List<Integer>> lists) {
+        for(List<Integer> list: lists) {
+            for (int i: list) {
+                System.out.print(i + " ");
+            }
             System.out.println();
         }
-
-        System.out.println("-------------------");
-        System.out.println(getAverage(parseInt) + "\t\t" + getAverage(valueOf));
-        printLastLine();
+        System.out.println();
     }
 
-    private static void printTime(long startTime, List<Integer> list) {
-        long stopTime = System.nanoTime();
-        int elapsedTime = (int) (stopTime - startTime);
-        list.add(elapsedTime);
-        System.out.print(elapsedTime + "\t");
-    }
+    private static Point findMax(List<List<Integer>> lists) {
+        Point point = new Point(0, 0);
+        int current = lists.get(0).get(0);
+        int max = current;
 
-    private static int getAverage(List<Integer> list) {
-        Integer sum = 0;
-        if (!list.isEmpty()) {
-            for (Integer mark : list) {
-                sum += mark;
-            }
-            return (int) (sum.doubleValue() / list.size());
-        }
-
-        return 0;
-    }
-
-    private static void printFibonacciNumbers(int n) {
-        int beforePrev = ZERO;
-        int prev = ONE;
-        int current;
-
-        for (int i = ZERO; i < n; i++) {
-            current = beforePrev + prev;
-            System.out.print(current + " ");
-
-            if (i != ZERO) {
-                beforePrev = prev;
-                prev = current;
+        for (int i = 0; i < lists.size(); i++) {
+            int pos = lists.get(i).get(i);
+            int neg = lists.get(lists.size() - i).get(i);
+            current = pos > neg ? pos : neg;
+            if(current > max) {
+                max = current;
+                point.setI(pos > neg ? i : lists.size() - i);
+                point.setJ(i);
             }
         }
 
-        printLastLine();
-    }
-
-    private static void printVampireNumbers() {
-        int counter = ONE;
-        for (int i = MIN; i < MAX; i++) {
-            for (int j = i; j < MAX; j++) {
-                int result = i * j;
-
-                List<Integer> fangs = (i + "" + j).chars().mapToObj(c -> c).sorted().collect(Collectors.toList());
-                List<Integer> number = (result + "").chars().mapToObj(c -> c).sorted().collect(Collectors.toList());
-
-                if (number.equals(fangs)) {
-                    System.out.println(counter++ + ") " + result + " = " + i + " * " + j);
-                }
-            }
-        }
-        printLastLine();
-    }
-
-    private static void printLastLine() {
-        System.out.println("\n********************\n\n");
+        return point;
     }
 }
