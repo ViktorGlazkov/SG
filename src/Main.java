ecfcd31bbddec620c5d96737e1a5bd465fb127b0
Viktor Glazkov <viktorg.dev@gmail.com>
2017-03-03 21:14:46 +0200

    Add executing.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
-import framework.Core;
-import framework.proxy.ProxyService;
+import area.Area;
+import area.AreaFactory;
+import area.AreaService;
+import fighter.Fighter;
+import fighter.FighterFactory;
+import printer.Printer;
 
-import java.util.Scanner;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        ProxyService.createProxies();
+    public static void main(String[] args) {
+        List<Fighter> fighterList = Collections.synchronizedList(FighterFactory.getFighterList(100));
+        List<Area> areas = AreaFactory.getAreas(10);
 
+        AreaService areaService = new AreaService(fighterList, areas);
+        areaService.startFight();
 
-        Scanner scanner = new Scanner(System.in);
-        String str = scanner.nextLine();
-        Core.execute(str);
+        Printer.printAreasStatistic(areas);
+        Printer.printBattleStatistic();
     }
 }
