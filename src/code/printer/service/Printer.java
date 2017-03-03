8c43b6eef04be2ea937b00edb6967e8f05921349
Viktor Glazkov <viktorg.dev@gmail.com>
2017-03-03 21:10:28 +0200

    Remove old code

--- src/code/printer/service/Printer.java
+++
@@ -1,6 +0,0 @@
-package code.printer.service;
-
-public interface Printer {
-    void print(String text);
-    void printReverted(String text);
-}
--- src/framework/annotation/Component.java
+++
@@ -1,9 +0,0 @@
-package framework.annotation;
-
-import java.lang.annotation.*;
-
-@Inherited
-@Target(ElementType.TYPE)
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Component {
-}
