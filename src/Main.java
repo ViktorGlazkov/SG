9a5d2a1cd8ec5b2b7e628df870ca5059b9840835
Viktor Glazkov <viktorg.dev@gmail.com>
2017-03-05 20:56:24 +0200

    Implement executor

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,9 @@
-import area.Area;
-import area.AreaFactory;
-import area.AreaService;
-import fighter.Fighter;
-import fighter.FighterFactory;
-import printer.Printer;
-
-import java.util.Collections;
-import java.util.List;
+import room.Room;
+import room.RoomService;
 
 public class Main {
     public static void main(String[] args) {
-        List<Fighter> fighterList = Collections.synchronizedList(FighterFactory.getFighterList(100));
-        List<Area> areas = AreaFactory.getAreas(10);
-
-        AreaService areaService = new AreaService(fighterList, areas);
-        areaService.startFight();
-
-        Printer.printAreasStatistic(areas);
-        Printer.printBattleStatistic();
+        Room room = new Room(2, 4);
+        RoomService.addUnits(100, room);
     }
 }
