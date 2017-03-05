0ca6125a281b4d4340ae9e7baef589cb29151742
Viktor Glazkov <viktorg.dev@gmail.com>
2017-03-05 20:56:07 +0200

    Add consts

---
+++ src/utils/Const.java
@@ -0,0 +1,12 @@
+package utils;
+
+public class Const {
+    public static final int ZERO = 0;
+    public static final int ONE = 1;
+    public static final int FIVE = 5;
+    public static final int HUNDRED = 100;
+    public static final int TWO_HUNDRED = 200;
+    public static final int THREE_HUNDRED = 300;
+    public static final int FOUR_HUNDRED = 400;
+    public static final int FIFE_HUNDRED = 500;
+}
